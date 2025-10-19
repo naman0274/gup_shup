1dbdd2ea8ac4c52f40d8da26a8d0476beb319906
Naman <namankumar0274@gmail.com>
2025-10-19 23:06:15 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:gup_shup/config/theme/app_theme.dart';
 import 'package:gup_shup/presentation/screens/auth/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
