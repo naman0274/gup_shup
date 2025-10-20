54e8075b77fe68d188e8fd87817733056435693d
Naman <namankumar0274@gmail.com>
2025-10-20 20:17:01 +0530

    dependencies added added app_router.dart

---
+++ lib/data/repositories/auth_repository.dart
@@ -0,0 +1,5 @@
+import 'package:gup_shup/data/services/base_repository.dart';
+
+class AuthRepository extends BaseRepository{
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gup_shup/config/theme/app_theme.dart';
+import 'package:gup_shup/data/repositories/auth_repository.dart';
 import 'package:gup_shup/presentation/screens/auth/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -38,5 +40,4 @@ class MyHomePage extends StatelessWidget{
     );
   }
 }
-
 
